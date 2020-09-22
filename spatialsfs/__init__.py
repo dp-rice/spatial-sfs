8d3a13fd0e9418ea8c0dd781fda3034102aa46ea
Daniel P. Rice <daniel.paul.rice@gmail.com>
2020-09-22 15:07:17 -0500

    Add some top-level imports.

--- spatialsfs/__init__.py
+++ spatialsfs/__init__.py
@@ -1,1 +1,4 @@
 """Calculating the Spatial SFS from branching diffusion simulations."""
+# flake8: noqa F401
+from spatialsfs.sampling import sample
+from spatialsfs.simulations import simulate_branching_diffusion
