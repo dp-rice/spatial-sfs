0d2889816aad235b7e62f1776b65379fc247f0f6
Daniel P. Rice <daniel.paul.rice@gmail.com>
2020-09-22 11:32:50 -0500

    Finish implementing sampling.

--- spatialsfs/sampling.py
+++ spatialsfs/sampling.py
@@ -1,5 +1,7 @@
 """Functions for sampling positions and the SFS."""
 
+from typing import Iterable, Iterator, Tuple
+
 import numpy as np
 from scipy.special import i0
 
@@ -47,7 +49,103 @@ def sample_positions(
     )
 
 
-def sample_weight(concentration: float, habitat_size: float, x: np.ndarray) -> float:
+def _generate_sample_positions(
+    branching_diffusion: BranchingDiffusion,
+    times: Iterable[float],
+    init_positions: Iterable[np.ndarray],
+    seed: np.random.SeedSequence,
+) -> Iterator[np.ndarray]:
+    for t, x0 in zip(times, init_positions):
+        yield sample_positions(branching_diffusion, t, seed) + x0
+
+
+def sample(
+    branching_diffusion: BranchingDiffusion,
+    num_samples: int,
+    concentration: float,
+    habitat_size: float,
+    seed,
+    num_centers: int = 1,
+    separation: float = 0.0,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Randomly generate the contributions to spatially localized samples.
+
+    Parameters
+    ----------
+    branching_diffusion : BranchingDiffusion
+        The branching diffusion to sample from.
+    num_samples : int
+        The number of (time, position) pairs to sample.
+    concentration : float
+        The concentration parameter `kappa` of the Von Mises sampling kernel
+    habitat_size : float
+        The linear size of the habitat.
+    seed :
+        A seed for numpy random number generation
+    num_centers : int
+        The number of concentrated sampling centers. Must be 1 or 2.
+        (default: 1)
+    separation : float
+        The distance separating two sampling centers.
+        (defaut: 0, ignored if num_centers == 1)
+
+    Returns
+    -------
+    intensities: np.ndarray
+        The number of individuals alive at sampled times weighted by the sampling kernel
+        intensities.shape == (num_samples, num_centers)
+    importance_weights
+        The weights on each sample from the initial position importance sampling.
+
+    """
+    if num_centers not in [1, 2]:
+        raise ValueError("num_centers must be 1 or 2.")
+    seed1, seed2, seed3 = _random.seed_handler(seed).spawn(3)
+    times = _random.sample_times(
+        num_samples, branching_diffusion.branching_process.final_time, seed1
+    )
+    init_positions, weights = _random.importance_sample_x0(
+        num_samples,
+        branching_diffusion.ndim,
+        _sampling_scale(branching_diffusion, concentration, separation),
+        seed2,
+    )
+    positions = _generate_sample_positions(
+        branching_diffusion, times, init_positions, seed3
+    )
+    intensities = _intensity(
+        positions, num_samples, concentration, habitat_size, num_centers, separation,
+    )
+    return intensities, weights
+
+
+def _sampling_scale(
+    bd: BranchingDiffusion, concentration: float, separation: float
+) -> float:
+    return 2 * max(bd.scale(), concentration ** (-1 / 2) + separation)
+
+
+def _intensity(
+    positions: Iterator[np.ndarray],
+    num_samples: int,
+    concentration: float,
+    habitat_size: float,
+    num_centers: int,
+    separation: float,
+) -> np.ndarray:
+    intensities = np.empty((num_samples, num_centers))
+    for i, x in enumerate(positions):
+        if num_centers == 1:
+            intensities[i] = sample_intensity(concentration, habitat_size, x)
+        elif num_centers == 2:
+            intensities[i] = two_sample_intensity(
+                concentration, habitat_size, x, separation
+            )
+    assert i == num_samples - 1
+    return intensities
+
+
+def sample_intensity(concentration: float, habitat_size: float, x: np.ndarray) -> float:
     """Compute the sample weight according to a Von Mises sampling kernel.
 
     Parameters
@@ -63,9 +161,39 @@ def sample_weight(concentration: float, habitat_size: float, x: np.ndarray) -> f
     Returns
     -------
     float
-        The sample weight
 
     """
     return np.sum(
         np.exp(concentration * np.sum(np.cos(4 * np.pi * x / habitat_size), axis=1))
     ) / (habitat_size * i0(concentration) / 2) ** (x.shape[1])
+
+
+def two_sample_intensity(
+    concentration: float, habitat_size: float, x: np.ndarray, separation: float
+) -> Tuple[float, float]:
+    """Compute the joint sample intensity for two sampling centers.
+
+    Parameters
+    ----------
+    concentration : float
+        The concentration parameter kappa of the Von Mises distribution.
+        The kernel approaches a Gaussian with sigma^2~kappa for large values of kappa
+    habitat_size : float
+        The linear size of the habitat.
+    x : np.ndarray
+        2D array of positions. Shape is (num_indivs, ndim)
+    separation : float
+        The linear separation between the sampling centers.
+
+    Returns
+    -------
+    Tuple[float, float]
+        (intensity_at_center1, intensity_at_center2)
+
+    """
+    translation = np.zeros(x.shape[1])
+    translation[0] = separation / 2
+    return (
+        sample_intensity(concentration, habitat_size, x + translation),
+        sample_intensity(concentration, habitat_size, x - translation),
+    )
--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -3,7 +3,13 @@ import numpy as np
 import pytest
 from scipy.stats import vonmises
 
-from spatialsfs.sampling import sample_positions, sample_weight
+from spatialsfs.sampling import (
+    _generate_sample_positions,
+    sample,
+    sample_intensity,
+    sample_positions,
+    two_sample_intensity,
+)
 
 
 def test_sample_positions(small_bd):
@@ -28,6 +34,45 @@ def test_sample_intensity(ndim, kappa):
     x = np.arange(12.0).reshape(-1, ndim)
     L = 5.0
     assert np.isclose(
-        sample_weight(kappa, L, x),
+        sample_intensity(kappa, L, x),
         np.sum(np.product(vonmises(kappa, scale=L / (4 * np.pi)).pdf(x), axis=1)),
     )
+
+
+@pytest.mark.parametrize("separation", [0.01, 1, 100.0])
+def test_two_sample_intensity(ndim, separation):
+    """Test symmetry of two_sample_intensity."""
+    x = np.zeros((1, ndim))
+    kappa = 1.0
+    L = 100.0
+    int1, int2 = two_sample_intensity(kappa, L, x, separation)
+    assert int1 == int2
+
+
+def test_generate_sample_positions(small_bd):
+    """Test generate_sample_positions iterator."""
+    times = [0.0, 0.25, 0.5]
+    seed = 1
+    x_0 = np.ones((len(times), small_bd.ndim))
+    gen = _generate_sample_positions(small_bd, times, x_0, seed)
+    # Should yield the same random values as the internal rng of gen
+    seedseq = np.random.SeedSequence(seed)
+    for t in times:
+        assert np.all(next(gen) == sample_positions(small_bd, t, seedseq) + 1.0)
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+@pytest.mark.parametrize("num_centers", [1, 2, 3])
+def test_sample(small_bd, num_centers):
+    """Test that sample runs and returns the correct shape."""
+    num_samples = 4
+    if num_centers > 2:
+        with pytest.raises(ValueError):
+            sample(small_bd, num_samples, 2.0, 100.0, 1, num_centers, 1.0)
+    else:
+        intensities, weights = sample(
+            small_bd, num_samples, 2.0, 100.0, 1, num_centers, 1.0
+        )
+        assert intensities.shape == (num_samples, num_centers)
+        assert weights.shape == (num_samples,)
